830986472c4c4e4ee9cf918ac098db6ae02ca2ab
unknown <oshamanbhany@gmail.com>
2025-11-23 23:57:37 +0530

    Created the Gray Button

--- src/Components/Buttons/BlueButton.js
+++ src/Components/Buttons/BlueButton.js
@@ -1,7 +1,10 @@
-export default function BlueButton({ text = "Launch App", height = 75 }) {
+export default function BlueButton({ text = "Launch App", height = 65 }) {
   return (
     <button
-      className="w-fit px-6 rounded-full text-white font-medium transition-all duration-300 hover:opacity-90"
+      className="w-fit px-8 rounded-full text-white font-medium transition-all 
+      duration-300 hover:opacity-100 hover:outline hover:outline-2 
+      hover:outline-[#6ec5ff] hover:shadow-[0_0_12px_white] 
+      hover:scale-105 active:scale-95"
       style={{
         background: "linear-gradient(90deg, #00C4FF, #006B89)",
         height: `${height}px`,
---
+++ src/Components/Buttons/GrayButton.js
@@ -0,0 +1,13 @@
+export function GrayButton({ text = "Learn More", height = 65 }) {
+  return (
+    <button
+      className="w-fit px-8 rounded-full text-white font-medium 
+      transition-all duration-300 bg-gray-500 hover:bg-gray-600 
+      hover:outline hover:outline-2 hover:outline-gray-400 hover:shadow-[0_0_12px_black] 
+      hover:scale-105 active:scale-95 active:bg-gray-700"
+      style={{ height: `${height}px` }}
+    >
+      {text}
+    </button>
+  );
+}
