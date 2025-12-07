fe0adde4f12fda52dd8d700c166df63bb3e15e04
zischl <147017426+zischl@users.noreply.github.com>
2025-12-07 22:21:00 +0530

    About-2 : better icons, cleanup and minor improvements

--- src/Pages/About-2/About.jsx
+++ src/Pages/About-2/About.jsx
@@ -1,13 +1,7 @@
-import { Clock, MapPin, Key, Timer, AlertTriangle, Wrench } from 'lucide-react';
+import { Snail, Skull, ShieldAlert, Timer, AlertTriangle, Wrench } from 'lucide-react';
 
 function AegisGlyph({ IconObject, title, desc }) {
   return (
-
-    // <div className="rounded-xl border border-cyan-400 p-5">
-    //   <div className="text-sky-400 text-3xl mb-3">{iconObject}</div>
-    //   <h3 className="text-white text-xl font-semibold mb-2">{title}</h3>
-    //   <p className="text-gray-400 text-sm">{desc}</p>
-    // </div>
     <div
       className={`
             w-50
@@ -25,6 +19,13 @@ function AegisGlyph({ IconObject, title, desc }) {
       }}>
 
       <div className="inset-0 flex flex-col items-center justify-center p-6 text-center">
+        <div
+          className={`
+          absolute inset-0
+          bg-gradient-to-b from-transparent via-white/5 to-transparent -translate-y-full 
+          group-hover:translate-y-full transition-transform duration-1000 pointer-events-none
+          `}>
+        </div>
         <div className={`w-14 h-14 bg-cyan-500/10 rounded-full flex items-center justify-center mb-3 border border-current/20 relative z-10`}>
           <IconObject className="w-7 h-7 text-cyan-400" />
         </div>
@@ -42,60 +43,65 @@ function AegisGlyph({ IconObject, title, desc }) {
 export default function About() {
   const problemPoints = [
     {
-      icon: Clock,
+      icon: Snail,
       title: "Slow Entry",
       description: "later..",
       color: "cyan"
     },
     {
-      icon: MapPin,
-      title: "Touchpoints",
+      icon: Skull,
+      title: "Unhygenic Contact",
       description: "later...",
       color: "red"
     },
     {
-      icon: Key,
+      icon: ShieldAlert,
       title: "Weak Credentials",
-      description: "Physical badges and cards can be easily lost, stolen, or duplicated, creating significant security gaps in access control systems.",
+      description: "later...",
       color: "pink"
     },
     {
       icon: Timer,
-      title: "Delays",
+      title: "Operational Delays",
       description: "later...",
       color: "cyan"
     },
     {
       icon: AlertTriangle,
-      title: "Risks",
+      title: "Security Risks",
       description: "later...",
       color: "red"
     },
     {
       icon: Wrench,
-      title: "Maintenance",
+      title: "High Maintenance",
       description: "later...",
       color: "purple"
     }
   ];
 
   return (
-    <section>
-      <div className="w-full">
-        <section className="text-center mb-20">
-          <h1 className="text-5xl font-bold mb-4 text-white">
-            The problem we saw
-          </h1>
-          <p className="text-xl text-gray-400">Why traditional systems fail</p>
-        </section>
-
-        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto w-full">
+    <section className="flex flex-col items-center">
+      <div className="text-center mb-20">
+        <h1 className="text-5xl font-bold mb-4 text-white">
+          The problem we saw
+        </h1>
+        <p className="text-xl text-gray-400">Why traditional systems fail</p>
+      </div>
+      <div className="bg-slate-900 rounded-xl px-24 py-10">
+        <div
+          className="
+          grid grid-cols-1
+          md:grid-cols-2
+          lg:grid-cols-3
+          gap-18
+          max-w-6xl w-full
+          mx-auto">
           {problemPoints.map((point, index) => (
             <AegisGlyph IconObject={point.icon} title={point.title} desc={point.description} />
           ))}
-        </section>
+        </div>
       </div>
-      {/*<AuraPane src="./src/palm-scan.png" alt="bleh" glowColor="cyan" glowRange="40px" />*/}
     </section>
   );
 }
