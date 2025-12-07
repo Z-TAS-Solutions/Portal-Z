262965bfb70a6ef846102359f5053b8b2dde6c32
zischl <147017426+zischl@users.noreply.github.com>
2025-12-07 09:19:35 +0530

    adding chat box card

--- src/Components/ChatBot/ChatBotWidget.jsx
+++ src/Components/ChatBot/ChatBotWidget.jsx
@@ -5,6 +5,45 @@ import { useState } from "react";
 import ChatBotInterface from "./ChatBot";
 import { TailwindColors as TColors } from "../../ColorPalette.jsx";
 
+function ChatBoxCard({
+  className = "",
+  fill = "",
+  strokeColor = "",
+  strokeWidth = 0,
+}) {
+  const w = 350;
+  const h = 520;
+  const r = 20;
+  const cutSize = 68;
+  const R_cut = 0;
+  const pathData = ` 
+      M ${r} 0 H ${w - r} Q ${w} 0 ${w} ${r}
+      V ${h - R_cut} 
+      A ${R_cut} ${R_cut} 0 0 1 ${w - R_cut} ${h}
+      H ${r} 
+      Q 0 ${h} 0 ${h - r} 
+      V ${r} 
+      Q 0 0 ${r} 0 
+      Z
+`;
+
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 100% 100%"
+      className={`overflow-visible ${className}`}
+    >
+      <path
+        d={pathData}
+        fill={fill}
+        stroke={strokeColor}
+        strokeWidth={strokeWidth}
+      />
+    </svg>
+  );
+}
+
 export default function ChatBotWidget({ buttonSize }) {
   const { chatInput, setChatInput, messages, status, onSubmit } =
     ChatBotInterface();
