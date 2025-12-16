6fb5c0e5b8b703b0f173f9fab82d105c37cb974d
zischl <147017426+zischl@users.noreply.github.com>
2025-12-16 21:28:56 +0530

    testing build again

--- src/app/Pages/HomePage/HomePage.jsx
+++ src/app/Pages/HomePage/HomePage.jsx
@@ -1,38 +1,33 @@
-import '../../CSS Files/HomePage.css';
+import "../../CSS Files/HomePage.css";
 
 // Imported components
-import MainHeading from '../../Components/Headings/MainHeading.jsx'; 
-import BlueButton from '../../Components/Buttons/BlueButton.jsx'; 
-import GrayButton from '../../Components/Buttons/GrayButton.jsx';
-import NormalParagraph from '../../Components/Paragraphs/NormalParagraph.jsx';
-
+import MainHeading from "../../components/Headings/MainHeading.jsx";
+import BlueButton from "../../components/Buttons/BlueButton.jsx";
+import GrayButton from "../../components/Buttons/GrayButton.jsx";
+import NormalParagraph from "../../components/Paragraphs/NormalParagraph.jsx";
 
 export default function Homepage() {
-    return (
-         <div className="hero-section">
-            <div className="angled-background"></div>
-            <div className="hero-content">
-                
-                
-                <h1 className="heading-sub">ZERO-TOUCH AUTHENTICATION</h1>
-                
-                
-                <MainHeading className="!text-7xl !font-extrabold !mb-4">
-                    Reinvented.
-                </MainHeading>
-                
-                
-                <NormalParagraph className="!text-xl !mt-0 !mb-10">
-                    Your identity secured through NIR vein biometrics and advanced cryptographic defense.
-                </NormalParagraph>
-                
-                <div className="cta-buttons">
-                    
-                    <BlueButton text="Launch App" />
-                    <GrayButton text="Learn More" />
-                </div>
-            </div>
+  return (
+    <div className="hero-section">
+      <div className="angled-background"></div>
+      <div className="hero-content">
+        <h1 className="heading-sub">ZERO-TOUCH AUTHENTICATION</h1>
+
+        <MainHeading className="!text-7xl !font-extrabold !mb-4">
+          Reinvented.
+        </MainHeading>
+
+        <NormalParagraph className="!text-xl !mt-0 !mb-10">
+          Your identity secured through NIR vein biometrics and advanced
+          cryptographic defense.
+        </NormalParagraph>
+
+        <div className="cta-buttons">
+          <BlueButton text="Launch App" />
+          <GrayButton text="Learn More" />
         </div>
-    );
-    
+      </div>
+    </div>
+  );
 }
+
