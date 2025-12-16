241fc43c1c2635965a97cf214378d5bfc70b3629
zischl <147017426+zischl@users.noreply.github.com>
2025-12-17 01:16:57 +0530

    quick fix for homepage z index issue

--- src/app/Pages/HomePage/HomePage.jsx
+++ src/app/Pages/HomePage/HomePage.jsx
@@ -6,28 +6,29 @@ import BlueButton from "../../components/Buttons/BlueButton.jsx";
 import GrayButton from "../../components/Buttons/GrayButton.jsx";
 import NormalParagraph from "../../components/Paragraphs/NormalParagraph.jsx";
 
-export default function Homepage() {
+export default function Homepage({ id = "home" }) {
   return (
-    <div className="hero-section">
-      <div className="angled-background"></div>
-      <div className="hero-content">
-        <h1 className="heading-sub">ZERO-TOUCH AUTHENTICATION</h1>
+    <section id="home">
+      <div className="hero-section">
+        <div className="angled-background"></div>
+        <div className="hero-content">
+          <h1 className="heading-sub">ZERO-TOUCH AUTHENTICATION</h1>
 
-        <MainHeading className="!text-7xl !font-extrabold !mb-4">
-          Reinvented.
-        </MainHeading>
+          <MainHeading className="!text-7xl !font-extrabold !mb-4">
+            Reinvented.
+          </MainHeading>
 
-        <NormalParagraph className="!text-xl !mt-0 !mb-10">
-          Your identity secured through NIR vein biometrics and advanced
-          cryptographic defense.
-        </NormalParagraph>
+          <NormalParagraph className="!text-xl !mt-0 !mb-10">
+            Your identity secured through NIR vein biometrics and advanced
+            cryptographic defense.
+          </NormalParagraph>
 
-        <div className="cta-buttons">
-          <BlueButton text="Launch App" />
-          <GrayButton text="Learn More" />
+          <div className="cta-buttons">
+            <BlueButton text="Launch App" />
+            <GrayButton text="Learn More" />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
-
